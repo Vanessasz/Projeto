5cd60fe1ae6ce0a96db789f2969c1bb3b958ed91
Kerenskybr <roger.o.monteiro@gmail.com>
2019-06-24 23:31:04 -0300

    Changes

--- controllers/rotas.py
+++ controllers/rotas.py
@@ -1,25 +1,24 @@
 
-import string
-from random import choice
-
 from Projeto import app, db #Aqui estou fazendo o importe da instância que eu criei.
 
-from Projeto.models import database
-from Projeto.models.database import desc, loja, administrador
+#from Projeto.models import database
+from Projeto.models.database import Desc, Administrador, Role, UserRoles
 
 from flask import render_template, url_for, flash, redirect, request, abort, send_file, Markup
 from flask_sqlalchemy import SQLAlchemy
 
-from flask_login import login_user, login_manager, current_user, logout_user, login_required
+from sqlalchemy import func
+
+from flask_user import current_user, login_required, roles_required, UserManager
 
 from Projeto.controllers.form import FormSistema, FormRegistro, FormLogin, LoginAdmin, SistemaBusca
 
+from flask_login import login_user, login_manager, logout_user
 
-db.create_all()
-db.session.commit()
 
-conn = sqlite3.connect("bancodedados.db")
-cursor = conn.cursor()
+
+
+
 
 @app.route("/")
 @app.route("/home")
@@ -27,14 +26,93 @@ def home():
 	""" Essa função renderiza a página principal """
 	return render_template('home.html')
 
+#Rota para o admin
+@app.route("/sistema_admin", methods=['GET', 'POST']) 
+	#Aqui definimos uma rota ṕara a página
+@roles_required('admin')
+	# Aqui o usuário só terá acesso ao sistema se estiver feito login
+def sistema_admin():
+	
+	lista_teste = []
+
+	for u in db.session.query(Administrador).all():
+	   
+	   lista_teste.append(str(u._id) + '  ' + u.nome)
 
 
+	print('LISTA TESTE', lista_teste)
+
+
+	##########################################################
+	'''
+	lojas_cadastradas = db.session.query(Administrador.nome).all()
+	
+	lista_loja = []
+
+	lista_loja = [x[0] for x in lojas_cadastradas]
+
+	print('LISTA LOJA',lista_loja)
+	'''
+	##########################################################3
+
+	form = FormSistema()
+	
+	if form.validate_on_submit():
+
+		campos = request.form.get('seletor_loja')
+
+		lista_extaida = []
+
+		lista_extaida = campos.split('  ')
+
+		print('LISTA EXTRAIDA', lista_extaida)
+		
+		valores = Desc(descricao=form.descricao.data, quantidade=form.quantidade.data, valor=form.valor.data, 
+						dia=form.dia.data, nomeloja=lista_extaida[1], descricoes_id=int(lista_extaida[0]))  
+
+		#nomeloja=request.form.get('seletor_loja')
+
+		db.session.add(valores)
+		#session usando os atributos
+		db.session.commit()
+		#salvando os dados definitivo no banco de dados
+		
+		flash(f'Adicionado com sucesso!', 'success')
+		print(valores)
+
+		return redirect(url_for('sistema_admin'))	
+
+	#lojas_cadastradas = db.session.query(Role.name=='user')
+	#lojas_cadastradas = db.session.query(Administrador).filter_by(nomeloja)
+
+	#Defini uma função para essa página
+
+	""" Instanciei meu formulário para poder usar """
+	#form_busca = SistemaBusca()
+	#busca
+
+	#Query para as somas
+	soma_itens = db.session.query(func.sum(Desc.quantidade)).first()
+	soma_valores = db.session.query(func.sum(Desc.valor)).first()
+
+	soma_total = db.session.query(func.sum(Desc.valor * Desc.quantidade)).first()#filter_by(descricoes_id=current_user._id)
+	
+	valores = 0
+
+	query = db.session.query(Desc).all()
+	#query = Desc.query.filter_by(descricoes_id=current_user._id)
+
+	#nomeloja = current_user._id
+
+	return render_template('sistema_admin.html',form=form, query=query, soma_itens=soma_itens[0], soma_valores=soma_valores[0],
+							 soma_total=soma_total[0], lista_teste=lista_teste) 
+
+
+#Rota para as lojas
 @app.route("/sistema", methods=['GET', 'POST']) 
 	#Aqui definimos uma rota ṕara a página
-@login_required 
+@login_required
 	# Aqui o usuário só terá acesso ao sistema se estiver feito login
-
-	
 def sistema(): 
 	#Defini uma função para essa página
 	formulario = FormSistema()
@@ -42,14 +120,18 @@ def sistema():
 	#form_busca = SistemaBusca()
 	#busca
 
+	#Query para as somas
+	soma_itens = db.session.query(func.sum(Desc.quantidade)).first()
+	soma_valores = db.session.query(func.sum(Desc.valor)).first()
 
+	soma_total = db.session.query(func.sum(Desc.valor * Desc.quantidade)).first()#filter_by(descricoes_id=current_user._id)
 	
 	valores = 0
 
-	query = desc.query.filter_by(descricoes_id=current_user._id)
+	query = Desc.query.filter_by(descricoes_id=current_user._id)
 	print('ID',query)
 
-	nomeloja = current_user._id
+	#nomeloja = current_user._id
 	print(current_user._id)
 
 	if formulario.validate_on_submit():
@@ -67,11 +149,9 @@ def sistema():
 
 		return redirect(url_for('sistema'))
 
-	return render_template('sistema.html', formulario=formulario, query=query) 
+	return render_template('sistema.html', formulario=formulario, query=query, soma_itens=soma_itens[0], 
+											soma_valores=soma_valores[0], soma_total=soma_total[0]) 
 
-def SistemaBusca():
-
-			
 
 @app.route("/registro", methods=['GET', 'POST'])
 def registro(): 	
@@ -80,16 +160,19 @@ def registro():
 
 	
 	if form.validate_on_submit():
-		valores = loja(nome=form.nome.data, telefone=form.telefone.data, cnpj=form.cnpj.data, 
-			email=form.email.data, senha=form.senha.data)
+		valores = Administrador(nome=form.nome.data, email=form.email.data, senha=form.senha.data)
 
 		db.session.add(valores)
+		regra = Role(name='user')
+		#regra = UserRoles(role_id=2)
+		valores.roles.append(regra)
 
 		db.session.commit()
 		flash(f'Cadastro efetuado com sucesso. Você pode agora se logar {form.nome.data}!', 'success')
 		return redirect(url_for('login'))
 
 	return render_template('registro.html', title='Register', form=form)
+
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -102,7 +185,7 @@ def login():
         return redirect(url_for('sistema'))
 
     if form.validate_on_submit():
-        query = loja.query.filter_by(email=form.email.data).first()
+        query = Administrador.query.filter_by(email=form.email.data).first()
         if query and query.senha == form.senha.data:
             login_user(query)
             #flash(f'Login efetuado com sucesso.', 'success')
@@ -112,7 +195,7 @@ def login():
             flash('Login inválido.', 'danger')
     return render_template('login.html', title='login', form=form)
 
-
+''''
 @app.route("/esqueceuasenha", methods=['GET', 'POST'])
 def esqueceuasenha():
 	
@@ -125,7 +208,7 @@ def esqueceuasenha():
 	flash('Uma senha foi enviada para o email cadastrado.' , 'success')
 	return redirect(url_for('login'))
 
-
+'''
 
 @app.route("/loginadmin", methods=['GET', 'POST'])
 def loginadmin():
@@ -134,33 +217,43 @@ def loginadmin():
 
     if current_user.is_authenticated:
         
-        return redirect(url_for('sistema'))
+        return redirect(url_for('sistema_admin'))
     
     if form.validate_on_submit():
-        query = administrador.query.filter_by(email=form.email.data).first()
+        query = Administrador.query.filter_by(email=form.email.data).first()
+        
         if query and query.senha == form.senha.data:
             login_user(query)
-            flash("Login efetuado com sucesso.")
-            return redirect(url_for('sistema'))
+            flash("Login efetuado com sucesso.", 'success')
+            return redirect(url_for('sistema_admin'))
         else:
             print(form.errors)          
             flash("Login inválido.", "text-danger")
     return render_template('loginadmin.html', title='login', form=form)
 
 
-
-
 @app.route("/visualizar/<ver_item>", methods=['GET', 'POST'])
 def visualizar(ver_item):
 
-	query = desc.query.get_or_404(ver_item)
+	#query = desc.query.get_or_404(ver_item).filter_by(lojas.descricoes_id)
+
+	query = db.session.query(ver_item).filter_by(Desc.descricoes_id)
+
+	soma_itens = db.session.query(func.sum(Desc.quantidade)).first()
+
+
+	loja = db.session.query(func.sum(Desc.valor)).filter_by(Desc.nomeloja)
+	#filter_by(descricoes_id=current_user._id)
+
+	#loja = des.query.filter_by(desc.nomeloja).first()
 	
-	return render_template('visualizar.html', query=query)
+	return render_template('visualizar.html', query=query, loja=loja)
+
 
 @app.route("/sistema/<item_id>/editar", methods=['GET', 'POST'])
 def editar(item_id):
 	
-	query_editar = desc.query.get_or_404(item_id)
+	query_editar = Desc.query.get_or_404(item_id)
 
 	print('TIPO NOMELOJA',type(query_editar.nomeloja))
 	print('TIPO DESC',type(query_editar.descricao))
@@ -204,7 +297,6 @@ def excluir(excluir_id):
 	return redirect(url_for('sistema', query_excluir = query_excluir))
 
 
-
 @app.route("/minhaconta", methods=['GET', 'POST'])
 @login_required
 def minhaconta():
@@ -233,15 +325,43 @@ def minhaconta():
 	return render_template('minhaconta.html', title='Minha Conta', form=form)
 
 
-
-
-
-
-
-
 @app.route("/sair")
 def sair():
     logout_user()
     return redirect(url_for('home'))
 
-			
+'''
+
+
+       <select name="seletor_loja" method="GET" class="custom-select" selected>
+                                {% for seletor_loja in lista_loja[1:] %}
+                                <option value="{{ seletor_loja }}">{{ seletor_loja }}</option>
+                                {% endfor %}
+
+                            </select>
+
+
+<select name="seletor_loja" method="GET" class="custom-select custom-select-sm">
+	{% for seletor_loja in lista_loja[1:] %}
+  <option selected>Selecione a Loja</option>
+  <option value="{{ seletor_loja }}">{{ seletor_loja }}</option>
+    {% endfor %}
+
+</select>
+
+'''
+
+
+'''
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+            <h6 class="text-left">Pesquisar itens</h6> </br>
+            <form action="SistemaBusca" class="navbar-pull-right">
+                <input type="text" class="span2">
+                <button class="btn btn-light btn-sm" href="#" method="GET">Buscar</button>
+            </form>
+        </div>
+    </div>
+
+'''
